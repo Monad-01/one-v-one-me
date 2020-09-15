d43163f9cece11df7753169324f9ef7ded2f762b
Johnathan Pruitt <pruittj1998@gmail.com>
2020-09-14 22:28:44 -0500

    Did some event stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1> Hello World! </h1>
+      <h1 onClick={() => console.log("You clicked rock")}> Rock </h1>
+      <h1> Paper </h1>
+      <h1> Scissors </h1>
     </div>
   );
 }
