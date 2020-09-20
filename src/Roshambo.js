0c2c3b4c389775aeeda6f93d1e4b757ae7bace2f
Johnathan Pruitt <pruittj1998@gmail.com>
2020-09-20 00:25:14 -0500

    added grid to divide

--- src/Roshambo.js
+++ src/Roshambo.js
@@ -1,12 +1,20 @@
 import React from "react";
+import "./Roshambo.css";
 import rockImage from "./Images/temp-rock.webp";
 
 function Roshambo() {
   return (
-    <div>
-      <img class="ro" src={rockImage} />
-      <h1> Paper </h1>
-      <h1> Scissors </h1>
+    <div class="game-container">
+      <div class="rock">
+        <h1>rock</h1>
+        <img src={rockImage} />
+      </div>
+      <div class="paper">
+        <h1>paper</h1>
+      </div>
+      <div class="scissors">
+        <h1>scissors</h1>
+      </div>
     </div>
   );
 }
